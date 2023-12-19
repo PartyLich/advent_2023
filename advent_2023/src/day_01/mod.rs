51d68b1a82a078e4c39463df9f6ec6b39efc3e6c
Arthur E. Jones <PartyLich@gmail.com>
2023-12-19 17:08:37 -0600

    test: add 1-2 test

--- advent_2023/src/day_01/mod.rs
+++ advent_2023/src/day_01/mod.rs
@@ -19,6 +19,14 @@ pub fn one(file_path: &str) -> usize {
     read_file(file_path).lines().map(parse_line).sum::<usize>()
 }
 
+/// Returns the sum of all of the calibration values. Calibration values can be found by combining
+/// the first digit and the last digit (in that order) to form a single two-digit number. Some of
+/// the digits are actually spelled out with letters: one, two, three, four, five, six, seven,
+/// eight, and nine also count as valid "digits".
+pub fn two(file_path: &str) -> usize {
+    0
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -28,6 +36,14 @@ mod test {
         let msg = "should return the sum of all of the calibration values";
         let expected = 142;
         let actual = one("input/01-t.txt");
+        assert_eq!(actual, expected, "{}", msg);
+    }
+
+    #[test]
+    fn part_two() {
+        let msg = "should return the sum of all of the calibration values";
+        let expected = 281;
+        let actual = two("input/01-t2.txt");
         assert_eq!(actual, expected, "{}", msg);
     }
 }
