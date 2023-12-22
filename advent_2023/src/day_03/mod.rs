3040fbbf8438fb21356395bbd09e14f83c8a7885
Arthur E. Jones <PartyLich@gmail.com>
2023-12-22 15:08:34 -0600

    test: add 3-1 test

---
+++ advent_2023/src/day_03/mod.rs
@@ -0,0 +1,20 @@
+//! Solutions to 2023 day 03 problems
+//! --- Day 3: Gear Ratios ---
+
+/// Returns the sum of the engine part numbers.
+pub fn one(file_path: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn part_one() {
+        let msg = "should return ";
+        let expected = 4361;
+        let actual = one("input/03-t.txt");
+        assert_eq!(actual, expected, "{}", msg);
+    }
+}
